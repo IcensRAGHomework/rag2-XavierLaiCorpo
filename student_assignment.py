08192d97cae0fb7ec210f3b7a6ed681adefe84b4
xavier_lai <xavier_lai@asus.com>
2025-02-19 13:24:58 +0800

    HW02 - v1

--- student_assignment.py
+++ student_assignment.py
@@ -5,9 +5,44 @@ from langchain_text_splitters import (CharacterTextSplitter,
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
+def load_with_PyPdf(file_path: str):
+    loader = PyPDFLoader(file_path)
+    return loader.load()
+
+def print_docs(docs):
+    page_num = 0
+    for doc in docs:
+        print("----- Doc", page_num, "begin ------")
+        print(f"Length={len(doc.page_content)}")
+        print("**** Page Content ****")
+        print(doc.page_content)
+        print("**********************")
+        #print("===== MetaData ====")
+        #print(doc.metadata)
+        #print("===================")
+        print("----- Doc", page_num, "end ------")
+        print()
+        page_num += 1
+
+def recursive_split_with_regex(text):
+    spliter = RecursiveCharacterTextSplitter(separators=["第 .+ 章 .+\n", "第 \\d+-?\\d* 條\n"],
+                                    chunk_size=0,
+                                    chunk_overlap=0,
+                                    is_separator_regex=True,
+                                    keep_separator=True)
+    result = spliter.create_documents([text], metadatas=[{"spliter": "Xavier_Lai"}])
+    #print_docs(result)
+    return result
 
 def hw02_1(q1_pdf):
-    pass
+    docs = load_with_PyPdf(q1_pdf)
+    return docs[-1]
 
 def hw02_2(q2_pdf):
-    pass
+    docs = load_with_PyPdf(q2_pdf)
+    text = ""
+    for doc in docs:
+        text = text + doc.page_content + "\n"
+    result = recursive_split_with_regex(text)
+    return len(result)
+
